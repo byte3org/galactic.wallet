6f7bc318215ce3f813351ea9e9fd88f816103b34
rxOred <rxored@gmail.com>
2023-08-20 20:12:42 +0530

    feat: wallet handlers

---
+++ api/v1/handlers/wallet.go
@@ -0,0 +1,120 @@
+package handlers
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+
+	"github.com/byte3/galactic.wallet/internal/database"
+	"github.com/byte3/galactic.wallet/internal/models"
+	"github.com/google/uuid"
+)
+
+type WalletHandler struct{}
+
+func (wh WalletHandler) CreateWallet(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Header.Get("Content-Type") != "application/json" {
+		msg := "Content-Type Header is not application/json"
+		http.Error(w, msg, http.StatusUnsupportedMediaType)
+		return
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var wallet models.WalletModel
+	err = json.Unmarshal(body, &wallet)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// check if wallet exist for the user
+	_, err = database.SelectWalletByUserId(wallet.ID)
+	if err == nil {
+		http.Error(w, "User already has a wallet", http.StatusConflict)
+	}
+
+	rows, err := database.CreateWallet(&wallet)
+	if err != nil || rows != 1 {
+		http.Error(w, "Failed to create entry", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusAccepted)
+}
+
+func (wh WalletHandler) GetWalletBalance(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func (wh WalletHandler) GetWalletTransactions(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+}
+
+func (wh WalletHandler) DepositToWallet(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+}
+
+func (wh WalletHandler) WithdrawFromWallet(w http.ResponseWriter, r *http.Request) {
+
+	userId := r.Context().Value("user_id").(uuid.UUID)
+	amount, err := strconv.Atoi(r.Context().Value("amount").(string))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	wallet, err := database.SelectWalletByUserId(userId)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if wallet.Balance > amount {
+		wallet.Balance -= amount
+		// insert into database
+		rows, err := database.UpdateWalletBalance(&wallet)
+		if err != nil || rows != 1 {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		payment := models.PaymentModel{
+			Amount:    amount,
+			IsSuccess: true,
+			WalletID:  wallet.ID,
+			Wallet:    wallet,
+		}
+		rows, err = database.CreatePayment(&payment)
+		if err != nil || rows != 1 {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	} else {
+		payment := models.PaymentModel{
+			Amount:    amount,
+			IsSuccess: false,
+			WalletID:  wallet.ID,
+			Wallet:    wallet,
+		}
+		rows, err := database.CreatePayment(&payment)
+		if err != nil || rows != 1 {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		http.Error(w, "Transaction failed. Not Enough Cash", 402)
+		return
+	}
+	w.WriteHeader(http.StatusAccepted)
+	w.Write([]byte("Payment Successful"))
+}
---
+++ api/v1/router/wallet.go
@@ -0,0 +1,27 @@
+package router
+
+import (
+	"github.com/byte3/galactic.wallet/api/v1/handlers"
+	"github.com/byte3/galactic.wallet/api/v1/middlewares"
+	"github.com/go-chi/chi"
+)
+
+type Wallet struct{}
+
+func (w Wallet) Routes() chi.Router {
+	r := chi.NewRouter()
+
+	r.Post("/create", handlers.WalletHandler{}.CreateWallet)
+	r.Group(func(r chi.Router) {
+		r.Use(middlewares.ExtractUserId)
+		r.Get("/user/balance", handlers.WalletHandler{}.GetWalletBalance)
+		r.Get("/user/transactions", handlers.WalletHandler{}.GetWalletTransactions)
+	})
+	r.Group(func(r chi.Router) {
+		r.Use(middlewares.ExtractUserId)
+		r.Use(middlewares.SignatureVerify)
+		r.Post("/user/deposit", handlers.WalletHandler{}.DepositToWallet)
+		r.Post("/user/withdraw", handlers.WalletHandler{}.WithdrawFromWallet)
+	})
+	return r
+}
